52ecafc2cc57c2e709841e9bd6237b566e476742
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-11-18 19:03:04 +0100

    refactor(tree_builder/commutatively_merge_lists): simplify

--- src/tree_builder.rs
+++ src/tree_builder.rs
@@ -611,18 +611,8 @@ impl<'a, 'b> TreeBuilder<'a, 'b> {
         debug!("{pad}commutatively_merge_lists");
         let raw_separator = commutative_parent.separator;
         let trimmed_sep = raw_separator.trim();
-        let trimmed_left_delim = commutative_parent
-            .left_delim
-            .as_ref()
-            .copied()
-            .unwrap_or("")
-            .trim();
-        let trimmed_right_delim = commutative_parent
-            .right_delim
-            .as_ref()
-            .copied()
-            .unwrap_or("")
-            .trim();
+        let trimmed_left_delim = commutative_parent.left_delim.unwrap_or_default().trim();
+        let trimmed_right_delim = commutative_parent.right_delim.unwrap_or_default().trim();
         // TODO improve handling of comments? comments added by the right side should ideally be placed sensibly
 
         // first, map each list via class mapping to make each element comparable
