04d972993ad8b329b8bd28f1625725b8d72f7f61
ugur-a <ugur-a@noreply.codeberg.org>
2024-12-04 08:35:43 +0000

    style (#76)
    Co-authored-by: Ughur Alakbarov <ugur.alekperov@gmail.com>
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/76
    Reviewed-by: Antonin Delpeuch <wetneb@noreply.codeberg.org>
    Co-authored-by: ugur-a <ugur-a@noreply.codeberg.org>
    Co-committed-by: ugur-a <ugur-a@noreply.codeberg.org>

--- src/bin/mergiraf.rs
+++ src/bin/mergiraf.rs
@@ -134,7 +134,7 @@ fn real_main(args: CliArgs) -> Result<i32, String> {
             right_name,
             compact,
         } => {
-            let old_git_detected = base_name.as_deref().unwrap_or("") == "%S";
+            let old_git_detected = base_name.as_deref().is_some_and(|n| n == "%S");
 
             let settings = DisplaySettings {
                 diff3: true,
@@ -238,30 +238,24 @@ fn real_main(args: CliArgs) -> Result<i32, String> {
                 &working_dir,
             );
             match postprocessed {
+                Ok(merged) if merged.method == "original" => 1,
                 Ok(merged) => {
-                    if merged.method == "original" {
-                        1
+                    if keep {
+                        print!(
+                            "{}",
+                            imitate_cr_lf_from_input(&original_conflict_contents, &merged.contents)
+                        );
                     } else {
-                        if keep {
-                            print!(
-                                "{}",
-                                imitate_cr_lf_from_input(
-                                    &original_conflict_contents,
-                                    &merged.contents
-                                )
-                            );
-                        } else {
-                            write_string_to_file(
-                                &(fname_conflicts.clone() + ".orig"),
-                                &conflict_contents,
-                            )?;
-                            write_string_to_file(&fname_conflicts, &merged.contents)?;
-                        };
-                        0
-                    }
+                        write_string_to_file(
+                            &(fname_conflicts.clone() + ".orig"),
+                            &conflict_contents,
+                        )?;
+                        write_string_to_file(&fname_conflicts, &merged.contents)?;
+                    };
+                    0
                 }
                 Err(e) => {
-                    warn!("Mergiraf: {}", e);
+                    warn!("Mergiraf: {e}");
                     1
                 }
             }
