b150910823d07ff6a08501869874c9ca54969fd5
Ada Alakbarova <ada.alakbarova@proton.me>
2025-02-10 11:58:44 +0000

    feat(line_based_merge_with_duplicate_signature_detection): if the line-merge has conflicts, reconstruct and check each revision (#195)
    And require all revisions to parse for the merge to be qualified as having no additional issues.
    
    fixes #194
    
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/195
    Reviewed-by: Antonin Delpeuch <wetneb@noreply.codeberg.org>
    Co-authored-by: Ada Alakbarova <ada.alakbarova@proton.me>
    Co-committed-by: Ada Alakbarova <ada.alakbarova@proton.me>

--- src/line_based.rs
+++ src/line_based.rs
@@ -1,4 +1,4 @@
-use crate::{parse, MergeResult, TSParser};
+use crate::{parse, pcs::Revision, MergeResult, TSParser};
 use diffy_imara::{Algorithm, ConflictStyle, MergeOptions};
 use typed_arena::Arena;
 
@@ -39,7 +39,7 @@ pub fn line_based_merge(
         conflict_count: parsed_merge.conflict_count(),
         conflict_mass: parsed_merge.conflict_mass(),
         method: LINE_BASED_METHOD,
-        has_additional_issues: false,
+        has_additional_issues: true,
     }
 }
 
@@ -55,26 +55,95 @@ pub(crate) fn line_based_merge_with_duplicate_signature_detection(
     let mut line_based_merge =
         line_based_merge(contents_base, contents_left, contents_right, Some(settings));
 
-    if line_based_merge.conflict_count == 0 {
-        let mut parser = TSParser::new();
-        parser
-            .set_language(&lang_profile.language)
-            .unwrap_or_else(|_| panic!("Error loading {} grammar", lang_profile.name));
+    let mut parser = TSParser::new();
+    parser
+        .set_language(&lang_profile.language)
+        .unwrap_or_else(|_| panic!("Error loading {} grammar", lang_profile.name));
+
+    let mut revision_has_issues = |contents: &str| {
         let arena = Arena::new();
         let ref_arena = Arena::new();
-        let tree_left = parse(
-            &mut parser,
-            &line_based_merge.contents,
+
+        let tree = parse(&mut parser, contents, lang_profile, &arena, &ref_arena);
+
+        tree.map_or(true, |ast| lang_profile.has_signature_conflicts(ast.root()))
+    };
+
+    let has_issues = if line_based_merge.conflict_count == 0 {
+        revision_has_issues(&line_based_merge.contents)
+    } else {
+        let parsed_merge = ParsedMerge::parse(&line_based_merge.contents, settings)
+            .expect("diffy-imara returned a merge that we cannot parse the conflicts of");
+
+        [Revision::Base, Revision::Left, Revision::Right]
+            .into_iter()
+            .map(|rev| parsed_merge.reconstruct_revision(rev))
+            .any(|contents| revision_has_issues(&contents))
+    };
+
+    line_based_merge.has_additional_issues = has_issues;
+
+    line_based_merge
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn some_reconstructed_revisions_do_not_parse() {
+        let contents_base = r#"import "github.com/go-redis/redis/v8"
+
+func foo(){}"#;
+
+        let contents_left = r#"import "github.com/redis/go-redis/v9"
+
+func foo(){}"#;
+
+        let contents_right = r#"import (
+	"fmt"
+	"net"
+	"net/url"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// a comment to split hunks
+func foo(){}"#;
+
+        let contents_expected = r#"<<<<<<< LEFT
+import "github.com/redis/go-redis/v9"
+||||||| BASE
+import "github.com/go-redis/redis/v8"
+=======
+import (
+	"fmt"
+	"net"
+	"net/url"
+>>>>>>> RIGHT
+
+	"github.com/redis/go-redis/v9"
+)
+
+// a comment to split hunks
+func foo(){}"#;
+
+        let lang_profile =
+            LangProfile::detect_from_filename("foo.go").expect("no `lang_profile` for Go");
+
+        let merge = line_based_merge_with_duplicate_signature_detection(
+            contents_base,
+            contents_left,
+            contents_right,
+            &Default::default(),
             lang_profile,
-            &arena,
-            &ref_arena,
         );
 
-        if let Ok(ast) = tree_left {
-            if lang_profile.has_signature_conflicts(ast.root()) {
-                line_based_merge.has_additional_issues = true;
-            }
-        }
+        assert_eq!(&merge.contents, contents_expected);
+
+        assert!(
+            merge.has_additional_issues,
+            "left and base reconstructed revisions shouldn't parse"
+        );
     }
-    line_based_merge
 }
