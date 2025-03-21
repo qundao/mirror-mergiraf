174ca2624fefd7e849ad76e4ca6070d1df4ec208
Ada Alakbarova <ada.alakbarova@proton.me>
2025-03-21 09:40:11 +0000

    test: test compact outputs if present (#275)
    One of the things decided in #225
    
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/275
    Reviewed-by: Antonin Delpeuch <wetneb@noreply.codeberg.org>
    Co-authored-by: Ada Alakbarova <ada.alakbarova@proton.me>
    Co-committed-by: Ada Alakbarova <ada.alakbarova@proton.me>

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -10,6 +10,43 @@ use rstest::rstest;
 mod common;
 use common::detect_extension;
 
+fn compare_against_merge(
+    test_dir: &Path,
+    fname_base: &'static Path,
+    contents_base: &'static str,
+    contents_left: &'static str,
+    contents_right: &'static str,
+    contents_expected: &str,
+    compact: bool,
+) {
+    let settings = DisplaySettings {
+        compact: Some(compact),
+        ..Default::default()
+    };
+
+    let merge_result = line_merge_and_structured_resolution(
+        contents_base,
+        contents_left,
+        contents_right,
+        fname_base,
+        settings,
+        true,
+        None,
+        None,
+        Duration::from_millis(0),
+    );
+
+    let expected = contents_expected.trim();
+    let actual = merge_result.contents.trim();
+    if expected != actual {
+        let patch = create_patch(expected, actual);
+        let f = PatchFormatter::new().with_color();
+        print!("{}", f.fmt_patch(&patch));
+        eprintln!("test failed: outputs differ for {}", test_dir.display());
+        panic!();
+    }
+}
+
 fn run_test_from_dir(test_dir: &Path) {
     let ext = detect_extension(test_dir);
     #[expect(unstable_name_collisions)]
@@ -25,29 +62,39 @@ fn run_test_from_dir(test_dir: &Path) {
     let contents_right = fs::read_to_string(fname_right)
         .expect("Unable to read right file")
         .leak();
-    let fname_expected = test_dir.join(format!("Expected.{ext}"));
-    let contents_expected = fs::read_to_string(fname_expected).expect("Unable to read right file");
 
-    let merge_result = line_merge_and_structured_resolution(
-        contents_base,
-        contents_left,
-        contents_right,
-        fname_base,
-        DisplaySettings::default(),
-        true,
-        None,
-        None,
-        Duration::from_millis(0),
-    );
+    {
+        let fname_expected = test_dir.join(format!("Expected.{ext}"));
+        let contents_expected =
+            fs::read_to_string(fname_expected).expect("Unable to read expected file");
 
-    let expected = contents_expected.trim();
-    let actual = merge_result.contents.trim();
-    if expected != actual {
-        let patch = create_patch(expected, actual);
-        let f = PatchFormatter::new().with_color();
-        print!("{}", f.fmt_patch(&patch));
-        eprintln!("test failed: outputs differ for {}", test_dir.display());
-        panic!();
+        compare_against_merge(
+            test_dir,
+            fname_base,
+            contents_base,
+            contents_left,
+            contents_right,
+            &contents_expected,
+            false,
+        );
+    }
+
+    {
+        // only run the following part if the file exists
+        let fname_expected_compact = test_dir.join(format!("ExpectedCompact.{ext}"));
+        let Ok(contents_expected_compact) = fs::read_to_string(fname_expected_compact) else {
+            return;
+        };
+
+        compare_against_merge(
+            test_dir,
+            fname_base,
+            contents_base,
+            contents_left,
+            contents_right,
+            &contents_expected_compact,
+            true,
+        );
     }
 }
 
