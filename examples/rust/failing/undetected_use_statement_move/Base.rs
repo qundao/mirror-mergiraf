58f0fe00056459bf1d969268e5fa676eecd89cfb
Antonin Delpeuch <antonin@delpeuch.eu>
2025-05-11 10:53:36 +0000

    tests: Add cases for Rust use statement matching (#378)
    Just adding the test cases (one working, one failing), no functional changes.
    
    For #139.
    
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/378
    Reviewed-by: Ada Alakbarova <ada4a@noreply.codeberg.org>
    Co-authored-by: Antonin Delpeuch <antonin@delpeuch.eu>
    Co-committed-by: Antonin Delpeuch <antonin@delpeuch.eu>

---
+++ examples/rust/failing/undetected_use_statement_move/Base.rs
@@ -0,0 +1,42 @@
+use ic_nervous_system_canisters::{cmc::CMCCanister, ledger::IcpLedgerCanister};
+use ic_nervous_system_clients::{
+    canister_status::CanisterStatusResultV2, ledger_client::LedgerCanister,
+};
+use ic_nervous_system_common::{
+    dfn_core_stable_mem_utils::{BufferedStableMemReader, BufferedStableMemWriter},
+    serve_logs, serve_logs_v2, serve_metrics,
+};
+use ic_nervous_system_proto::pb::v1::{
+    GetTimersRequest, GetTimersResponse, ResetTimersRequest, ResetTimersResponse, Timers,
+};
+use ic_nervous_system_runtime::CdkRuntime;
+use ic_nns_constants::LEDGER_CANISTER_ID as NNS_LEDGER_CANISTER_ID;
+use ic_sns_governance::{
+    governance::{
+        log_prefix, Governance, TimeWarp, ValidGovernanceProto, MATURITY_DISBURSEMENT_DELAY_SECONDS,
+    },
+    logs::{ERROR, INFO},
+    pb::v1 as sns_gov_pb,
+    types::{Environment, HeapGrowthPotential},
+    upgrade_journal::serve_journal,
+};
+use ic_sns_governance_api::pb::v1::{
+    get_running_sns_version_response::UpgradeInProgress, governance::Version,
+    ClaimSwapNeuronsRequest, ClaimSwapNeuronsResponse, FailStuckUpgradeInProgressRequest,
+    FailStuckUpgradeInProgressResponse, GetMaturityModulationRequest,
+    GetMaturityModulationResponse, GetMetadataRequest, GetMetadataResponse, GetMode,
+    GetModeResponse, GetNeuron, GetNeuronResponse, GetProposal, GetProposalResponse,
+    GetRunningSnsVersionRequest, GetRunningSnsVersionResponse,
+    GetSnsInitializationParametersRequest, GetSnsInitializationParametersResponse,
+    GetUpgradeJournalRequest, GetUpgradeJournalResponse, ListNervousSystemFunctionsResponse,
+    ListNeurons, ListNeuronsResponse, ListProposals, ListProposalsResponse, ManageNeuron,
+    ManageNeuronResponse, NervousSystemParameters, RewardEvent, SetMode, SetModeResponse,
+};
+#[cfg(feature = "test")]
+use ic_sns_governance_api::pb::v1::{
+    AddMaturityRequest, AddMaturityResponse, AdvanceTargetVersionRequest,
+    AdvanceTargetVersionResponse, GovernanceError, MintTokensRequest, MintTokensResponse, Neuron,
+    RefreshCachedUpgradeStepsRequest, RefreshCachedUpgradeStepsResponse,
+};
+#[cfg(test)]
+mod tests;
---
+++ examples/rust/working/move_use_statement/Expected.rs
@@ -0,0 +1,34 @@
+use ic_nns_constants::LEDGER_CANISTER_ID as NNS_LEDGER_CANISTER_ID;
+use ic_sns_governance::{
+    governance::{
+        log_prefix, Governance, TimeWarp, ValidGovernanceProto, MATURITY_DISBURSEMENT_DELAY_SECONDS,
+    },
+    logs::{ERROR, INFO},
+    pb::v1 as sns_gov_pb,
+    types::{Environment, HeapGrowthPotential},
+    upgrade_journal::serve_journal,
+};
+#[cfg(feature = "test")]
+use ic_sns_governance_api::pb::v1::{
+    AddMaturityRequest, AddMaturityResponse, AdvanceTargetVersionRequest,
+    AdvanceTargetVersionResponse, GovernanceError, MintTokensRequest, MintTokensResponse, Neuron,
+    RefreshCachedUpgradeStepsRequest, RefreshCachedUpgradeStepsResponse,
+};
+use ic_sns_governance_api::{
+    pb::v1::{
+        get_running_sns_version_response::UpgradeInProgress, governance::Version,
+        ClaimSwapNeuronsRequest, ClaimSwapNeuronsResponse, FailStuckUpgradeInProgressRequest,
+        FailStuckUpgradeInProgressResponse, GetMaturityModulationRequest,
+        GetMaturityModulationResponse, GetMetadataRequest, GetMetadataResponse, GetMode,
+        GetModeResponse, GetNeuron, GetNeuronResponse, GetProposal, GetProposalResponse,
+        GetRunningSnsVersionRequest, GetRunningSnsVersionResponse,
+        GetSnsInitializationParametersRequest, GetSnsInitializationParametersResponse,
+        GetUpgradeJournalRequest, GetUpgradeJournalResponse, Governance as GovernanceApi,
+        ListNervousSystemFunctionsResponse, ListNeurons, ListNeuronsResponse, ListProposals,
+        ListProposalsResponse, ManageNeuron, ManageNeuronResponse, NervousSystemParameters,
+        RewardEvent, SetMode, SetModeResponse,
+    },
+    topics::{ListTopicsRequest, ListTopicsResponse},
+};
+#[cfg(test)]
+mod tests;
