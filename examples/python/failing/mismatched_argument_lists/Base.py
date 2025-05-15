b652e801b2edcb95741351d93c18eb23d8222e0c
Antonin Delpeuch <antonin@delpeuch.eu>
2025-05-15 14:35:45 +0000

    tests: Add failing case for Python argument lists (#389)
    For #388.
    
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/389
    Reviewed-by: Ada Alakbarova <ada4a@noreply.codeberg.org>
    Co-authored-by: Antonin Delpeuch <antonin@delpeuch.eu>
    Co-committed-by: Antonin Delpeuch <antonin@delpeuch.eu>

---
+++ examples/python/failing/mismatched_argument_lists/Base.py
@@ -0,0 +1,5 @@
+from bookmaker import front_page, compile
+
+def main():
+    compile()
+
---
+++ examples/python/failing/mismatched_argument_lists/Left.py
@@ -0,0 +1,5 @@
+from bookmaker import front_page, compile, table_of_contents
+
+def main():
+    compile(toc=table_of_contents())
+
