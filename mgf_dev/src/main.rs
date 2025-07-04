e5ec40eb1cc841d29f521fea4c5fc84e2cda2363
Antonin Delpeuch <antonin@delpeuch.eu>
2025-07-04 11:39:13 +0200

    Make max_failures and max_steps configurable

--- mgf_dev/src/main.rs
+++ mgf_dev/src/main.rs
@@ -60,6 +60,12 @@ enum Command {
         /// Seed for all randomness involved
         #[arg(long)]
         seed: Option<u64>,
+        /// Maximum number of minimization steps to take
+        #[arg(long, default_value_t = 100)]
+        max_steps: i32,
+        /// Maximum number of failures to accept when attempting a minimization step
+        #[arg(long, default_value_t = 100)]
+        max_failures: i32,
     },
 }
 
@@ -137,6 +143,8 @@ fn real_main(args: &CliArgs) -> Result<i32, String> {
             expected_exit_code,
             output,
             seed,
+            max_steps,
+            max_failures,
         } => {
             minimize(
                 test_case,
@@ -144,6 +152,8 @@ fn real_main(args: &CliArgs) -> Result<i32, String> {
                 *expected_exit_code,
                 output.as_ref(),
                 *seed,
+                *max_steps,
+                *max_failures,
             );
             0
         }
