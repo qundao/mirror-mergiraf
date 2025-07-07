a9be189a12ec7acb5d5e41635a6bf652140bc7c4
Antonin Delpeuch <antonin@delpeuch.eu>
2025-07-07 15:33:38 +0200

    tests: Update helpers to support explicit language setting (#475)
    Updates the testing helpers to support test cases with a `language` file in them.
    
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/475
    Reviewed-by: Ada Alakbarova <ada4a@noreply.codeberg.org>
    Co-authored-by: Antonin Delpeuch <antonin@delpeuch.eu>
    Co-committed-by: Antonin Delpeuch <antonin@delpeuch.eu>

--- helpers/inspect.sh
+++ helpers/inspect.sh
@@ -10,37 +10,37 @@ script_path="$(realpath "${BASH_SOURCE[0]}")"
 script_dir="$(dirname "${script_path}")"
 
 # infer the extension
-ext=`ls $1 | grep Base. | sed -e 's/Base.//'`
+ext=`ls $1 | grep Base | sed -e 's/Base//'`
 
-${script_dir}/run.sh $1 | sed -e '$a\' > /tmp/out$$.$ext
+${script_dir}/run.sh $1 | sed -e '$a\' > /tmp/out$$$ext
 
 echo "------ RESULT ------"
-cat /tmp/out$$.$ext
+cat /tmp/out$$$ext
 
 echo "------ BASE --------"
-cat $1/Base.$ext | sed -e '$a\'
+cat $1/Base$ext | sed -e '$a\'
 echo "------ LEFT --------"
-cat $1/Left.$ext | sed -e '$a\'
+cat $1/Left$ext | sed -e '$a\'
 echo "------ RIGHT --------"
-cat $1/Right.$ext | sed -e '$a\'
+cat $1/Right$ext | sed -e '$a\'
 echo "------ EXPECTED --------"
-if [ -e $1/Better.$ext ]; then
-        cat $1/Better.$ext | sed -e '$a\' | tee /tmp/expected$$.$ext
-elif [ -e $1/ExpectedIdeally.$ext ]; then
-        cat $1/ExpectedIdeally.$ext | sed -e '$a\' | tee /tmp/expected$$.$ext
+if [ -e $1/Better$ext ]; then
+        cat $1/Better$ext | sed -e '$a\' | tee /tmp/expected$$$ext
+elif [ -e $1/ExpectedIdeally$ext ]; then
+        cat $1/ExpectedIdeally$ext | sed -e '$a\' | tee /tmp/expected$$$ext
 else
-        cat $1/Expected.$ext | sed -e '$a\' | tee /tmp/expected$$.$ext
+        cat $1/Expected$ext | sed -e '$a\' | tee /tmp/expected$$$ext
 fi
 echo "------ diff ------"
-diff -C 3 --color=auto /tmp/expected$$.$ext /tmp/out$$.$ext
+diff -C 3 --color=auto /tmp/expected$$$ext /tmp/out$$$ext
 successful=$?
 
 if [[ $successful -ne 0 ]]; then
 
-    if cargo run -p mgf_dev -- compare --commutative /tmp/expected$$.$ext /tmp/out$$.$ext > /dev/null 2>&1; then
+    if cargo run -p mgf_dev -- compare --commutative /tmp/expected$$$ext /tmp/out$$$ext > /dev/null 2>&1; then
        echo "------"
        echo "Note: up to formatting differences, the test passes"
     fi
 fi
 
-rm /tmp/expected$$.$ext /tmp/out$$.$ext
+rm /tmp/expected$$$ext /tmp/out$$$ext
--- helpers/run.sh
+++ helpers/run.sh
@@ -4,7 +4,7 @@
 script_path="$(realpath "${BASH_SOURCE[0]}")"
 script_dir="$(dirname "${script_path}")"
 
-ext=`ls $1 | grep Base. | sed -e 's/Base.//'`
+ext=`ls $1 | grep Base | sed -e 's/Base//'`
 
 mkdir -p debug
 extra_args="-v -d debug/"
@@ -12,4 +12,9 @@ if [ "$NO_DEBUG" == "true" ]; then
     extra_args=""
 fi
 
-${script_dir}/../target/debug/mergiraf merge $1/Base.$ext $1/Left.$ext $1/Right.$ext -s BASE -x LEFT -y RIGHT $extra_args
+if [ -e $1/language ]; then
+    language=`cat $1/language`
+    extra_args="$extra_args --language $language"
+fi
+
+${script_dir}/../target/debug/mergiraf merge $1/Base$ext $1/Left$ext $1/Right$ext -s BASE -x LEFT -y RIGHT $extra_args
