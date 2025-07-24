3aef884f1c0fe769186f257abf9ecf174bc30955
Antonin Delpeuch <antonin@delpeuch.eu>
2025-07-24 07:41:25 +0200

    tests: Utilities to benchmark mergiraf on large corpora

---
+++ helpers/benchmark.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: $0 <path> <executable>"
+    echo "where <path> is a path to a directory containing test cases, such as examples/java/working,"
+    echo "and <executable> is a path to a mergiraf binary to benchmark."
+    echo "This prints a TSV file summarizing benchmarking results on the standard output."
+    echo "It can then be used with helpers/summarize_benchmark.py to get statistics in Markdown."
+    exit 1
+fi
+
+suite=$1
+executable=$2
+
+script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_dir="$(dirname "${script_path}")"
+
+tmp_dir=/tmp/tmp$$
+mkdir -p $tmp_dir
+
+# Check that a given file can be parsed using our own parser
+check_parsing() {
+    cargo run --release -p mgf_dev -- parse $1 $extra_args > /dev/null 2>&1
+    return $?
+}
+
+# Print TSV headers
+echo -e "status\ttiming\tlanguage\tcase"
+
+# For each test case…
+
+find -L $suite -type d | while read -r testid ; do
+    # Detect its language
+    ext=`ls $testid | grep Base | sed -e 's/Base//'`
+    if [ ! -e "$testid/Expected$ext" ]; then
+        continue
+    fi
+
+    language="*$ext"
+    extra_args=""
+    if [ -e $testid/language ]; then
+        language=`cat $testid/language`
+        extra_args="$extra_args --language $language"
+    fi
+
+    # Run the executable to benchmark
+    /usr/bin/time -o $tmp_dir/timings -f "%e" ${executable} merge $testid/Base$ext $testid/Left$ext $testid/Right$ext -s BASE -x LEFT -y RIGHT $extra_args > $tmp_dir/our_merge_raw$ext 2> /dev/null
+    retcode=$?
+
+    # Normalize line endings both for the expected value and our output
+    cat $tmp_dir/our_merge_raw$ext | sed -e '$a\' > $tmp_dir/our_merge$ext
+    cat "$testid/Expected$ext" | sed -e '$a\' > $tmp_dir/expected_merge$ext
+
+    timing=`tail -1 $tmp_dir/timings`
+    
+    # Categorize the test outcome
+    if [ $retcode -ge 128 ]; then
+        outcome="Panic"
+    elif diff -B $tmp_dir/our_merge$ext $tmp_dir/expected_merge$ext > /dev/null 2>&1; then
+        outcome="Exact"
+    else
+        conflict_count=`cat $tmp_dir/our_merge_raw$ext | grep "<<<<<<<" | wc -l`
+        if [ $conflict_count -ge 1 ]; then
+            # Check that we were able to parse the files correctly
+            if check_parsing $testid/Base$ext && check_parsing $testid/Left$ext && check_parsing $testid/Right$ext; then
+                outcome="Conflict"
+            else
+                outcome="Parse"
+            fi
+        elif cargo run --release -p mgf_dev -- compare --commutative $tmp_dir/our_merge$ext $tmp_dir/expected_merge$ext > /dev/null 2>&1; then
+            outcome="Format"
+        else
+            outcome="Differ"
+        fi;
+    fi;
+    echo -e "${outcome}\t${timing}\t${language}\t${testid}"
+done;
+
+rm -rf ${tmp_dir}
---
+++ helpers/summarize_benchmark.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+import sys
+import csv
+import math
+from collections import defaultdict
+
+"""
+Utility that summarizes the outcomes of a benchmark run with the `helpers/benchmark.sh` script.
+When supplied with the path to a single benchmark TSV log, it gives statistics about how many
+test cases ended up in different categories, as a Markdown table.
+When supplied with two such paths, it generates
+- a comparison between the logs, as a similar table
+- a confusion matrix indicating from which status to which status test cases changed
+"""
+
+# The order in which test case categories should be presented
+# Roughly from "best to worst", so that the confusion matrix
+# can be intuitively interpreted as satisfying if it is lower-triangular
+status_order = ['Exact', 'Format', 'Conflict', 'Differ', 'Parse', 'Panic']
+
+def to_dict(generator):
+    """
+    Takes a stream from a TSV file, eats the first row as a header
+    and yields the rest of rows as dictionaries instead of lists
+    """
+    header = next(generator)
+    for row in generator:
+        yield dict(zip(header, row))
+
+class TimingStats:
+    """
+    Utility to compute an average out of many data points.
+    We could also compute the standard deviation by computing the sum of squares.
+    """
+    def __init__(self):
+        self.count = 0
+        self.sum = 0
+
+    def add(self, timing):
+        self.count += 1
+        self.sum += timing
+
+    def average(self):
+        return float(self.sum) / self.count
+
+class StatsLine:
+    """
+    Statistics about how many test cases land in each status category,
+    and how long they took.
+    """
+    def __init__(self):
+        self.timing = TimingStats()
+        self.states = defaultdict(int)
+
+    def add(self, case):
+        timing = float(case['timing'])
+        self.timing.add(timing)
+        status = case['status']
+        self.states[status] += 1
+
+    def to_markdown(self):
+        cases = self.timing.count
+        parts = [f"{cases:,}"]
+        for status in status_order:
+            count = self.states[status]
+            if count:
+                parts.append(f"{count:,} ({100 * count / cases:.0f}%)")
+            else:
+                parts.append("0")
+
+        timing = self.timing.average()
+        parts.append(f"{timing:.3f}")
+        return '| ' + (' | '.join(parts)) + ' |'
+
+class StatsDiff:
+    """
+    Difference between two `StatsLine` from two different benchmark runs
+    """
+    def __init__(self, first: StatsLine, second: StatsLine):
+        self.first = first
+        self.second = second
+
+    def to_markdown(self):
+        timing = self.second.timing.average()
+        timing_diff = timing - self.first.timing.average()
+        parts = [f"{self.second.timing.count:,}"]
+        for status in status_order:
+            first = self.first.states[status]
+            second = self.second.states[status]
+            if second - first and self.first.timing.count:
+                percent_change = 100 * (second - first) / self.first.timing.count
+                parts.append(f"{second - first:+,} **({percent_change:+.1f}%)**")
+            else:
+                parts.append(f"{second - first:+,}")
+
+        if math.fabs(timing_diff) > 0.001:
+            if timing > 0:
+                percent_change = 100 * timing_diff / self.first.timing.average()
+                parts.append(f"{timing_diff:+.3f} ({percent_change:+.1f}%)")
+            else:
+                parts.append(f"{timing_diff:+.3f}%)")
+        else:
+            parts.append(f"{timing:.3f}")
+        return '| ' + (' | '.join(parts)) + ' |'
+
+class BenchmarkLog:
+    def __init__(self, path: str, restrict_to=None):
+        """
+        Parses a benchmark log as a TSV file,
+        returning global and per-language statistics.
+        If another BenchmarkLog is passed as `restrict_to`,
+        then only cases which are also covered by the other
+        log are taken into account.
+        """
+        self.global_stats = StatsLine()
+        self.per_language = defaultdict(StatsLine)
+        self.case_to_status = {}
+        with open(path, 'r') as f:
+            csv_reader = csv.reader(f, delimiter='\t')
+            for case in to_dict(csv_reader):
+                if restrict_to is None or case['case'] in restrict_to.case_to_status:
+                    self.global_stats.add(case)
+                    self.per_language[case['language']].add(case)
+                    self.case_to_status[case['case']] = case['status']
+
+def print_header():
+    """
+    Prints the header of a Markdown table representing test case categories
+    """
+    print('| Language | Cases | '+ ' | '.join(status_order)  + ' | Time (s) |')
+    print('| -------- | ----- | '+ ' | '.join(['-' * len(status) for status in status_order ]) + ' | -------- |')
+
+def summarize_benchmark_log(path: str):
+    """
+    Prints a summary of a single benchmark
+    """
+    print_header()
+    log = BenchmarkLog(path)
+    lang_stats = list(log.per_language.items())
+    lang_stats.sort(key=lambda pair: -pair[1].timing.count)
+    for lang, stats in lang_stats:
+        print(f"| `{lang}` {stats.to_markdown()}")
+
+    if len(log.per_language) > 1:
+        print(f"| **Total** {log.global_stats.to_markdown()}")
+
+
+def compare_benchmark_logs(path_before: str, path_after: str):
+    """
+    Prints a summary of the differences between two benchmark runs
+    """
+    print_header()
+    after_log = BenchmarkLog(path_after)
+    before_log = BenchmarkLog(path_before, restrict_to=after_log)
+    after_lang_stats = list(after_log.per_language.items())
+    after_lang_stats.sort(key=lambda pair: -pair[1].timing.count)
+    for lang, stats in after_lang_stats:
+        diff = StatsDiff(before_log.per_language[lang], stats)
+        print(f"| `{lang}` {diff.to_markdown()}")
+
+    if len(after_log.per_language) > 1:
+        global_diff = StatsDiff(before_log.global_stats, after_log.global_stats)
+        print(f"| **Total** {global_diff.to_markdown()}")
+
+    print('')
+
+    confusion_matrix = defaultdict(set)
+    for case, status in after_log.case_to_status.items():
+        previous_status = before_log.case_to_status.get(case)
+        if not previous_status:
+            print(f"warning: case not found in previous benchmark: {case}", file=sys.stderr)
+        else:
+            confusion_matrix[(previous_status, status)].add(case)
+
+    print('| ↓ Before \\ After → | '+ ' | '.join(status_order)  + ' |')
+    print('| ------------------ | '+ ' | '.join(['-' * len(status) for status in status_order ]) + ' |')
+    for status in status_order:
+        row = []
+        for new_status in status_order:
+            cell = ''
+            if confusion_matrix[(status, new_status)]:
+                cell = f"{len(confusion_matrix[(status, new_status)]):,}"
+            if cell and status != new_status:
+                cell = f"**{cell}**"
+            row.append(cell)
+        print('| '+status+ ' | ' + ' | '.join(row) + ' |')
+
+    print('')
+    print('## Suspicious status changes')
+    print('')
+    for index, status in enumerate(status_order):
+        for new_status in status_order[(index+1):]:
+            cases = confusion_matrix[(status, new_status)]
+            if cases:
+                print(f"### {status} → {new_status}")
+                for case in cases:
+                    print(case)
+                print('')
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        compare_benchmark_logs(sys.argv[1], sys.argv[2])
+    else:
+        summarize_benchmark_log(sys.argv[1])
