60a0762ab8834bfa4b33d938aa1e4f85f7899900
OvidiusCicero <ovidiuscicero@noreply.codeberg.org>
2026-06-30 17:29:52 +0200

    tests(CMake): add reformatting example (#776)
    I found a second scenario where our limited CMake support actually helps. Not sure I like mergirafs resolution but it was actually helpful at work
    
    Co-authored-by: Christian Hufnagel <christian.hufnagel@zuhlke.com>
    Reviewed-on: https://codeberg.org/mergiraf/mergiraf/pulls/776
    Reviewed-by: Antonin Delpeuch <wetneb@noreply.codeberg.org>

---
+++ examples/cmake/working/reformattting/Expected.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 3.15...4.0)
+project(
+  ModernCMakeExample
+  VERSION 1.0
+  LANGUAGES CXX)
+add_executable(exe
+    a.cpp
+    b.cpp
+    c.cpp
+    d.cpp
+    e.cpp
+    f.cpp
+    g.cpp
+    h.cpp
+    i.cpp
+    j.cpp
+    k.cpp new.cpp
+)
---
+++ examples/cmake/working/reformattting/Left.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.15...4.0)
+project(
+  ModernCMakeExample
+  VERSION 1.0
+  LANGUAGES CXX)
+add_executable(
+    exe
+    a.cpp
+    b.cpp
+    c.cpp
+    d.cpp
+    e.cpp
+    f.cpp
+    g.cpp
+    h.cpp
+    i.cpp
+    j.cpp
+    k.cpp
+)
